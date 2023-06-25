0ecbd9220d59d1bb855b29afecc0e09a3789ca89
nickmoline <nickmoline@users.noreply.github.com>
2023-06-25 23:38:51 +0000

    deploy: 99e90f2720a8ccfaa205c22627ef0d2802cc68d5

--- nickmoline.routines.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-     $(window).resize(function(){
-         if($(this).width() >= 767){
-             $.backstretch("/optimized/portraitleft.jpg", {speed: 150});
-         }
-      })
-      .resize();//trigger resize on page load
-});
